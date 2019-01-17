e9a8bbdadcac666321e3005c2c3952ea62ddf291
Yann Pouillon <devops@materialsevolution.es>
2019-01-17 16:46:22 +0100

    Adjusted the configuration of Sphinx to build the existing pages

--- doc/conf.py
+++ doc/conf.py
@@ -34,7 +34,8 @@
 extensions = ['sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.mathjax',
-    'sphinx.ext.ifconfig']
+    'sphinx.ext.ifconfig',
+    'sphinxcontrib.mermaid']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
