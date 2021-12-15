f32229605a3c9d4289e69ba2fbaff1346f05f9c7
alin m elena <alinm.elena@gmail.com>
2021-12-15 10:25:17 +0000

    add valgrind to ci

--- .ci/docker/debian/build.sh
+++ .ci/docker/debian/build.sh
@@ -2,5 +2,7 @@
 
 set -x
 
+#docker pull debian:buster
 docker pull debian:bullseye
+#docker build -t debian-esl:buster . 
 docker build -t debian-esl:bullseye . 
--- .ci/docker/debian/upload.sh
+++ .ci/docker/debian/upload.sh
@@ -1,2 +1,4 @@
 docker tag debian-esl:bullseye eslib/esl-0.7.x:debian-bullseye
+#docker tag debian-esl:buster eslib/esl-0.6.x:debian-buster
 docker push eslib/esl-0.7.x:debian-bullseye
+#docker push eslib/esl-0.6.x:debian-buster
